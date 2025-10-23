161ddac9fe88201bcc4799f7ca7a302024544359
Lanchô <projetofinal303@gmail.com>
2025-10-23 16:49:17 -0300

    fix: padroniza table_name pedido em minúsculo para compatibilidade BD

--- xampp/htdocs/models/pedido.php
+++ xampp/htdocs/models/pedido.php
@@ -1,7 +1,7 @@
 <?php
 class Pedido {
     private $conn;
-    private $table_name = "Pedido";
+    private $table_name = "pedido";
 
     public $id;
     public $id_cliente;
@@ -33,19 +33,16 @@ class Pedido {
     function create() {
         $query = "INSERT INTO " . $this->table_name . " SET id_cliente=:id_cliente, descricao=:descricao, valor=:valor, data=:data, status=:status";
         $stmt = $this->conn->prepare($query);
-
         $this->id_cliente=htmlspecialchars(strip_tags($this->id_cliente));
         $this->descricao=htmlspecialchars(strip_tags($this->descricao));
         $this->valor=htmlspecialchars(strip_tags($this->valor));
         $this->data = date('Y-m-d H:i:s'); // Data atual
         $this->status="Pendente";
-
         $stmt->bindParam(":id_cliente", $this->id_cliente);
         $stmt->bindParam(":descricao", $this->descricao);
         $stmt->bindParam(":valor", $this->valor);
         $stmt->bindParam(":data", $this->data);
         $stmt->bindParam(":status", $this->status);
-
         if($stmt->execute()) {
             $this->id = $this->conn->lastInsertId();
             return true;
@@ -56,13 +53,10 @@ class Pedido {
     function update_status() {
         $query = "UPDATE " . $this->table_name . " SET status = :status WHERE id = :id";
         $stmt = $this->conn->prepare($query);
-
         $this->status=htmlspecialchars(strip_tags($this->status));
         $this->id=htmlspecialchars(strip_tags($this->id));
-
         $stmt->bindParam(':status', $this->status);
         $stmt->bindParam(':id', $this->id);
-
         if($stmt->execute()) {
             return true;
         }
@@ -74,7 +68,6 @@ class Pedido {
         $stmt = $this->conn->prepare($query);
         $this->id=htmlspecialchars(strip_tags($this->id));
         $stmt->bindParam(1, $this->id);
-
         if($stmt->execute()) {
             return true;
         }
