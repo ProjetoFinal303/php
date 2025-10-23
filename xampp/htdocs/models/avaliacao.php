f28ad818ec66b51d839dceb7aec992995501f409
Lanchô <projetofinal303@gmail.com>
2025-10-23 16:56:31 -0300

    fix: padroniza JOIN cliente em minúsculo em avaliacao.php

--- xampp/htdocs/models/avaliacao.php
+++ xampp/htdocs/models/avaliacao.php
@@ -18,19 +18,16 @@ class Avaliacao {
     function create() {
         $query = "INSERT INTO " . $this->table_name . " SET produto_id=:produto_id, cliente_id=:cliente_id, nota=:nota, comentario=:comentario";
         $stmt = $this->conn->prepare($query);
-
         // Limpeza de dados
         $this->produto_id=htmlspecialchars(strip_tags($this->produto_id));
         $this->cliente_id=htmlspecialchars(strip_tags($this->cliente_id));
         $this->nota=htmlspecialchars(strip_tags($this->nota));
         $this->comentario=htmlspecialchars(strip_tags($this->comentario));
-
         // Binding
         $stmt->bindParam(":produto_id", $this->produto_id);
         $stmt->bindParam(":cliente_id", $this->cliente_id);
         $stmt->bindParam(":nota", $this->nota);
         $stmt->bindParam(":comentario", $this->comentario);
-
         if($stmt->execute()) {
             return true;
         }
@@ -41,7 +38,7 @@ class Avaliacao {
     function readByProduto() {
         $query = "SELECT a.id, a.nota, a.comentario, a.created_at, c.nome as nome_cliente, c.avatar_url 
                   FROM " . $this->table_name . " a 
-                  JOIN Cliente c ON a.cliente_id = c.id
+                  JOIN cliente c ON a.cliente_id = c.id
                   WHERE a.produto_id = ? 
                   ORDER BY a.created_at DESC";
         
