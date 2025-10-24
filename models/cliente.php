9044dd1fa9c22991efcb5f6c53b3813986e3f6a1
Lanchô <projetofinal303@gmail.com>
2025-10-24 18:03:17 -0300

    Create Cliente class for client management

---
+++ models/cliente.php
@@ -0,0 +1,78 @@
+<?php
+class Cliente {
+    private $conn;
+    private $table_name = "clientes";
+
+    public $id;
+    public $nome;
+    public $email;
+    public $telefone;
+    public $endereco;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    function read() {
+        $query = "SELECT * FROM " . $this->table_name . " ORDER BY id DESC";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    function create() {
+        $query = "INSERT INTO " . $this->table_name . " (nome, email, telefone, endereco) VALUES (:nome, :email, :telefone, :endereco)";
+        $stmt = $this->conn->prepare($query);
+
+        $this->nome = htmlspecialchars(strip_tags($this->nome));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->telefone = htmlspecialchars(strip_tags($this->telefone));
+        $this->endereco = htmlspecialchars(strip_tags($this->endereco));
+
+        $stmt->bindParam(":nome", $this->nome);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":telefone", $this->telefone);
+        $stmt->bindParam(":endereco", $this->endereco);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    function update() {
+        $query = "UPDATE " . $this->table_name . " SET nome = :nome, email = :email, telefone = :telefone, endereco = :endereco WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+
+        $this->nome = htmlspecialchars(strip_tags($this->nome));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->telefone = htmlspecialchars(strip_tags($this->telefone));
+        $this->endereco = htmlspecialchars(strip_tags($this->endereco));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(":nome", $this->nome);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":telefone", $this->telefone);
+        $stmt->bindParam(":endereco", $this->endereco);
+        $stmt->bindParam(":id", $this->id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    function delete() {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $stmt->bindParam(":id", $this->id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+}
+?>
