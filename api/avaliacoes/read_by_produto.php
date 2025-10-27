034aadecec00c7c88ff0ab7b3656c922e13bf2c3
Lanchô <projetofinal303@gmail.com>
2025-10-27 19:29:35 -0300

    Disable error display in read_by_produto.php
    Suppress error reporting for production environment.

--- api/avaliacoes/read_by_produto.php
+++ api/avaliacoes/read_by_produto.php
@@ -1,6 +1,10 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: *');
 
